94cf95818808fa5f9a08d9ad2f9d265873b0aade
Anton Keks <anton@codeborne.com>
2014-05-29 00:50:23 +0300

    split to two lines

--- src/XML.java
+++ src/XML.java
@@ -1,16 +1,14 @@
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 public class XML {
   public static DocumentBuilder newBuilder() throws ParserConfigurationException {
-    return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    return factory.newDocumentBuilder();
   }
 
   public static Document parse(String xml) throws Exception {
